185b54cb8287151df1f517b6d4d332c7cbc6a753
Evgeny Petrov <golovasteek@gmail.com>
2020-06-21 16:12:36 +0200

    add isimage test (#2)

--- test_local.py
+++ test_local.py
@@ -1,5 +1,6 @@
 import uploader
 
 
-def test_dummy():
-    uploader.isimage('/')
+def test_isimage():
+    assert uploader.isimage('test_data/IMG_8076.JPG')
+    assert not uploader.isimage('test_data/test.txt')
